dff6927e51c5e9e8f4ce6e188eac5d3e857a4f63
AlexBurkh <alex.burkh@yandex.ru>
2022-09-20 01:06:49 +0300

    Обновлен bot.py

--- src/bot.py
+++ src/bot.py
@@ -6,57 +6,58 @@ from controller import controller
 class bot():
     __app = None
     __token = None
-    _control = controller()
+    __control = None
 
     def __init__(self, token):
         self.__token = token
+        self.__control = controller()
 
     async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        self._control.load_saved_data()
+        self.__control.load_saved_data()
         await self.menu(update, context)
 
     async def menu(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        answer = self._control.load_menu()
+        answer = self.__control.load_menu()
         await update.message.reply_text(answer)
 
     async def print(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        answer = self._control.load_print_book()
+        answer = self.__control.load_print_book()
         await update.message.reply_text(answer)
 
     async def add(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         contact_data = update.message.text.split()
-        answer = self._control.add_contact(
+        answer = self.__control.add_contact(
             contact_data[1], contact_data[2], contact_data[3], contact_data[4])
         await update.message.reply_text(answer)
 
     async def edit(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.edit_contact(msg[1:])
+        answer = self.__control.edit_contact(msg[1:])
         await update.message.reply_text(answer)
 
     async def delete(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.delete_contact(msg[1])
+        answer = self.__control.delete_contact(msg[1])
         await update.message.reply_text(answer)
 
     async def search_by_id(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.search_by_id(msg[1])
+        answer = self.__control.search_by_id(msg[1])
         await update.message.reply_text(answer)
 
     async def search_by_surname(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.search_by_surname(msg[1])
+        answer = self.__control.search_by_surname(msg[1])
         await update.message.reply_text(answer)
 
     async def import_contacts(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.import_contacts(msg[1])
+        answer = self.__control.import_contacts(msg[1])
         await update.message.reply_text(answer)
 
     async def export_contacts(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         msg = update.message.text.split()
-        answer = self._control.export_contacts(msg[1])
+        answer = self.__control.export_contacts(msg[1])
         await update.message.reply_text(answer)
 
     def run(self):
